8b6a2ed8cfeed693dbdd55e9daf260a0c71dd324
Anish Kumar Vedant <aveda2@unh,newhaven.edu>
2024-04-30 10:44:08 -0400

    Bob communication

--- Bob.py
+++ Bob.py
@@ -1,9 +1,8 @@
 import socket
-import binascii
+import binascii, os
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import padding, hmac, hashes
-import os
 
 def encrypt_message(message, shared_key, iv):
     cipher = Cipher(algorithms.AES(shared_key), modes.CBC(iv), backend=default_backend())
